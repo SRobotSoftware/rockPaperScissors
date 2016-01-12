5a8904ffd4aec1511b60f59bb72cc7766da4c157
Zmixon91 <zmixon91@gmail.com>
2016-01-12 02:46:49 -0700

    trimming and fixing history

--- rps.js
+++ rps.js
@@ -4,64 +4,50 @@ var gameHistoryElem = document.getElementById('gameHistory');
 var gameTitlePanelElem = document.getElementById('gameTitlePanel');
 var gameTextElem = document.getElementById('gameText');
 var gameImageElem = document.getElementById('gameImage');
-var gameState = null;
 
 // write functions
-
-function getCompChoice() {
-    // Pick at random
-    // 1=rock,2=paper,3=scissors
-    return Math.floor(Math.random() * 3) + 1;
-}
-
 function gameWrite(state) {
     // Writes results to page
     gameTextElem.innerText = "You " + state;
     switch (state) {
         case "WIN":
-        gameTitlePanelElem.classList.remove("panel-primary");
-        gameTitlePanelElem.classList.remove("panel-warning");
-        gameTitlePanelElem.classList.remove("panel-danger");
-        gameTitlePanelElem.classList.add("panel-success");
-        break;
+            // Still probably a better way to do this...
+            gameTitlePanelElem.setAttribute("class","panel panel-success");
+            break;
         case "STALEMATE":
-        gameTitlePanelElem.classList.remove("panel-primary");
-        gameTitlePanelElem.classList.remove("panel-danger");
-        gameTitlePanelElem.classList.remove("panel-success");
-        gameTitlePanelElem.classList.add("panel-warning");
-        break;
+            gameTitlePanelElem.setAttribute("class","panel panel-warning");
+            break;
         case "LOSE":
-        gameTitlePanelElem.classList.remove("panel-primary");
-        gameTitlePanelElem.classList.remove("panel-warning");
-        gameTitlePanelElem.classList.remove("panel-success");
-        gameTitlePanelElem.classList.add("panel-danger");
-        break;
+            gameTitlePanelElem.setAttribute("class","panel panel-danger");
+            break;
     }
 }
 
-function runGame(newChoice) {
+function runGame(playerChoice) {
     // Change the game image to players choice
-    gameImageElem.setAttribute("src", "img/" + newChoice.toLowerCase() + ".png");
+    gameImageElem.setAttribute("src", "img/" + playerChoice.toLowerCase() + ".png");
     // Get Computer Choice
-    var computerChoice = getCompChoice();
+    // Pick at random
+    // 1=rock,2=paper,3=scissors
+    var computerChoice = Math.floor(Math.random() * 3) + 1;
     // Allow us to get human readable output
     var computerText = {
         1: "ROCK",
         2: "PAPER",
         3: "SCISSORS"
     }
-    // Process Player Choice !!! Kinda redundant now? I'll fix later !!! This should be rewritten probably maybe numbers?
-    var playerChoice = newChoice;
+    // Process Player Choice !!! Kinda redundant now? I'll fix later
     var playerChoiceObj = {
         ROCK: 3,
         PAPER: 1,
         SCISSORS: 2
     }
     // Write to the game History so we can see what everyone got
-    gameHistoryElem.innerHTML += "<br />";
-    gameHistoryElem.innerText += "Computer: " + computerText[computerChoice] + " Player: " + playerChoice;
-    // Actual game logic !!! I could probably rewrite this with numbers in the objects to determine a stalemate
-    
+    var newItem = document.createElement("DIV");
+    var divInner = document.createTextNode("Computer: " + computerText[computerChoice] + " Player: " + playerChoice);
+    newItem.appendChild(divInner);
+    gameHistoryElem.insertBefore(newItem, gameHistoryElem.childNodes[0]);
+    // Actual game logic
     if (playerChoiceObj[playerChoice] === computerChoice) {
         gameWrite("WIN");
     } else if (playerChoice === computerText[computerChoice]) {
